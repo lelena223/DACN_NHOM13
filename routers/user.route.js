d40adaabe1a4f5f34bc470090a2867da2adaaab7
lena223 <lelenahkht2003@gamil.com>
2024-04-08 14:54:25 +0700

    :hammer: add user validation

--- routers/user.route.js
+++ routers/user.route.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const userRoute = express.Router();
 
-const { getUsers, getUserById, createUser, updateUser, deleteUser } = require("../controllers/user.controller")
+const { getUsers, getUserById, createUser, updateUser, deleteUser } = require("../controllers/user.controller");
+const validate = require("../middlewares/validate.middleware");
+const { userValidation } = require("../validators");
 
-userRoute.route("/").get(getUsers).post(createUser)
+userRoute.route("/").get(getUsers).post(validate(userValidation.createUser.body) ,createUser)
 
 userRoute.route("/:userId").get(getUserById).delete(deleteUser).patch(updateUser)
 
